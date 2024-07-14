65ad3bb23d306d674644e76e53ad3b18619270b1
apooravm <medal.apoorav@gmail.com>
2024-07-14 19:49:29 +0530

    When uploading: user enters target folder -> uploads. When downloading: user enters target folder -> downloads. Working on this next

--- go.mod
+++ go.mod
@@ -3,13 +3,13 @@ module github.com/apooravm/folder-sync-S3
 go 1.21.3
 
 require (
-	github.com/aws/aws-sdk-go-v2 v1.30.3
 	github.com/aws/aws-sdk-go-v2/config v1.27.26
 	github.com/aws/aws-sdk-go-v2/feature/s3/manager v1.17.7
 	github.com/aws/aws-sdk-go-v2/service/s3 v1.58.2
 )
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.30.3 // indirect
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.3 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.17.26 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.11 // indirect
--- src/main.go
+++ src/main.go
@@ -7,10 +7,11 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
+	"sync"
 
 	configMng "github.com/apooravm/folder-sync-S3/src/config"
 	"github.com/apooravm/folder-sync-S3/src/utils"
-	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 
 	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
@@ -19,12 +20,13 @@ import (
 )
 
 var (
-	s3Config      *configMng.S3_Config
-	configName    = "s3Config.json"
-	configPath    string
-	targetPath    string
-	LargeFileSize int64 = 100_1000_1000 // 100MB
-	client        *s3.Client
+	s3Config       *configMng.S3_Config
+	configName     = "s3Config.json"
+	configPath     string
+	targetPath     string
+	LargeFileSize  int64 = 100_1000_1000 // 100MB
+	client         *s3.Client
+	BaseObjectPath = "sync/"
 )
 
 func main() {
@@ -63,7 +65,7 @@ func main() {
 }
 
 // https://github.com/awsdocs/aws-doc-sdk-examples/blob/main/gov2/s3/actions/bucket_basics.go
-func GetObjectKeys() (*[]string, error) {
+func GetObjectKeys() (*[]FileInfo, error) {
 	cfg, err := config.LoadDefaultConfig(context.TODO())
 	if err != nil {
 		return nil, fmt.Errorf("Error loading default config. %s", err.Error())
@@ -79,9 +81,20 @@ func GetObjectKeys() (*[]string, error) {
 		return nil, fmt.Errorf("Could not list objects. %s", err.Error())
 	}
 
-	var objectKeySlice []string
+	var objectKeySlice []FileInfo
 	for _, item := range res.Contents {
-		objectKeySlice = append(objectKeySlice, string(*item.Key))
+		// Skip keys that are empty dirs.
+		// Only add keys pointing to a file.
+		if string(*item.Key)[len(*item.Key)-1] == '/' {
+			continue
+		}
+		fileBaseName := filepath.Base(string(*item.Key))
+
+		objectKeySlice = append(objectKeySlice, FileInfo{
+			Filepath:  "./downloads/" + fileBaseName,
+			Size:      *item.Size,
+			ObjectKey: string(*item.Key),
+		})
 	}
 
 	return &objectKeySlice, nil
@@ -137,75 +150,167 @@ func UploadFile(fileToUpload string) error {
 		return fmt.Errorf("Could not find target info. %s", err.Error())
 	}
 
+	// Dir upload handled in same func and arg
 	if targetPathInfo.IsDir() {
-		return fmt.Errorf("Target is not a file. Dir upload unavailable.")
+		var userConfirmRes string
+		fmt.Printf("Uploading folder %s\nContinue? (y/n)", targetPathInfo.Name())
+		fmt.Scan(&userConfirmRes)
+
+		if userConfirmRes == "y" || userConfirmRes == "yes" || userConfirmRes == "Yes" || userConfirmRes == "Y" {
+			if err := UploadDir(fileToUpload); err != nil {
+				return err
+			}
+		} else {
+			fmt.Println(utils.LogColourSprintf("Aborted", "red", false))
+		}
+
+		return nil
 	}
 
 	// Creating object key from filename
 	targetPathObjectKey := "public/folder_sync/" + targetPathInfo.Name()
 
-	fmt.Printf("Uploading file %s (%.2fMB) to %s\n", targetPathInfo.Name(), float64(targetPathInfo.Size())/float64(1000_000), targetPathObjectKey)
+	var userConfirmRes string
+	fmt.Printf("Uploading file %s (%.2fMB) to %s\nContinue? (y/n)\n", targetPathInfo.Name(), float64(targetPathInfo.Size())/float64(1000_000), targetPathObjectKey)
+	fmt.Scan(&userConfirmRes)
 
-	// Depending on the size of file, choose the upload method
-	// Right now a large file is ~100MB
-	if targetPathInfo.Size() <= LargeFileSize {
-		if err = UploadNormalFile(fileToUpload, targetPathObjectKey); err != nil {
-			return fmt.Errorf("Could not upload. %s", err.Error())
+	if userConfirmRes == "y" || userConfirmRes == "yes" || userConfirmRes == "Yes" || userConfirmRes == "Y" {
+		// Depending on the size of file, choose the upload method
+		// Right now a large file is ~100MB
+		if targetPathInfo.Size() <= LargeFileSize {
+			if err = UploadNormalFile(fileToUpload, targetPathObjectKey); err != nil {
+				return fmt.Errorf("Could not upload. %s", err.Error())
+			}
+
+		} else {
+			if err = UploadLargeFile(fileToUpload, targetPathObjectKey); err != nil {
+				return fmt.Errorf("Could not upload. %s", err.Error())
+			}
 		}
-
+		utils.ColourPrint(fmt.Sprintf("File uploaded successfully %s", targetPathObjectKey), "green")
 	} else {
-		if err = UploadLargeFile(fileToUpload, targetPathObjectKey); err != nil {
-			return fmt.Errorf("Could not upload. %s", err.Error())
-		}
+		fmt.Println(utils.LogColourSprintf("Aborted", "red", false))
 	}
 
 	return nil
 }
 
+type FileInfo struct {
+	Filepath  string
+	Size      int64
+	ObjectKey string
+}
+
 func UploadDir(pathToUpload string) error {
-	var dirFilePaths []string
-	fmt.Println(filepath.Base(pathToUpload))
+	var dirFileInfo []FileInfo
+	folderName := filepath.Base(pathToUpload)
+
 	filepath.Walk(pathToUpload, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 
 		if !info.IsDir() {
-			dirFilePaths = append(dirFilePaths, path)
+			// Modify path to start from the targetFolder
+			// Lots of string manipulation, could be slow
+			path_parts := strings.Split(filepath.ToSlash(path), "/")
+
+			splitIdx := 0
+			for i := 0; i < len(path_parts); i++ {
+				if path_parts[i] == folderName {
+					splitIdx = i
+					break
+				}
+			}
+
+			absFilePath, err := filepath.Abs(path)
+			if err != nil {
+				return fmt.Errorf("Could not get abs filepath. %s", err.Error())
+			}
+
+			pathRelativeToTargetFolder := strings.Join(path_parts[splitIdx:], "/")
+
+			dirFileInfo = append(dirFileInfo, FileInfo{
+				Filepath:  absFilePath,
+				Size:      info.Size(),
+				ObjectKey: BaseObjectPath + pathRelativeToTargetFolder,
+			})
 		}
 
 		return nil
 	})
 
-	for _, path := range dirFilePaths {
-		fmt.Println(path)
+	var wg sync.WaitGroup
+
+	// Btw, this creates a buffer channel with a certain capacity
+	// allowing a limited number of values to be sent to the channel without requiring a corresponding receive.
+	errChan := make(chan error, len(dirFileInfo))
+
+	for _, file := range dirFileInfo {
+		wg.Add(1)
+
+		// Passing in file pointer triggers some bug due to how goroutines handle variables
+		// Bug makes only the last file be uploaded multiple, len(dirFileInfo) times.
+		// GPT exp
+		// The issue with only the last file being uploaded could be due to a problem with
+		// how the goroutines are handling the file information.
+		// Specifically, the loop variable file is being captured by the goroutine,
+		// leading to a common concurrency bug in Go.
+		// In Go, loop variables are reused across iterations,
+		// so all goroutines end up using the same file value unless it's passed explicitly to the goroutine.
+
+		go func(file FileInfo) {
+			defer wg.Done()
+
+			if file.Size >= LargeFileSize {
+				if err := UploadLargeFile(file.Filepath, file.ObjectKey); err != nil {
+					errChan <- fmt.Errorf("%s could not be uploaded. %w", file.Filepath, err)
+				} else {
+					utils.ColourPrint("Upload successful "+file.ObjectKey, "green")
+				}
+			} else {
+				if err := UploadNormalFile(file.Filepath, file.ObjectKey); err != nil {
+					errChan <- fmt.Errorf("%s could not be uploaded. %w", file.Filepath, err)
+				} else {
+					utils.ColourPrint("Upload successful "+file.ObjectKey, "green")
+				}
+			}
+		}(file)
+	}
+
+	wg.Wait()
+	close(errChan)
+
+	for err := range errChan {
+		utils.ColourPrint(err.Error(), "red")
 	}
 
 	return nil
 }
 
-func ObjectKeyExists(objectKeyToCheck string) (bool, error) {
+// Fetches objectkeys and checks whether the input one exists
+func ObjectKeyExists(objectKeyToCheck string) (*FileInfo, error) {
 	objectKeySlice, err := GetObjectKeys()
 	if err != nil {
-		return false, err
+		return nil, err
 	}
 
 	for _, objKey := range *objectKeySlice {
-		if objectKeyToCheck == objKey {
-			return true, nil
+		if objectKeyToCheck == objKey.ObjectKey {
+			return &objKey, nil
 		}
 	}
 
-	return false, nil
+	return nil, nil
 }
 
 func DeleteFile(objectKeyToDelete string) error {
-	exists, err := ObjectKeyExists(objectKeyToDelete)
+	ObjInfo, err := ObjectKeyExists(objectKeyToDelete)
 	if err != nil {
 		return err
 	}
 
-	if !exists {
+	if ObjInfo == nil {
 		return fmt.Errorf("Object key does not exist in the bucket.")
 	}
 
@@ -286,23 +391,27 @@ func handleCliArgs() error {
 	case "generate":
 
 	case "download":
-		cfg, err := configMng.ReadConfig(configPath)
-		if err != nil {
-			return fmt.Errorf("Error reading config. %s", err.Error())
+		if err := os.MkdirAll("./downloads", os.ModePerm); err != nil {
+			return fmt.Errorf("Could not create downloads folder. %s", err.Error())
 		}
 
-		s3Config = cfg
+		if len(os.Args) < 3 {
+			return fmt.Errorf("No object key provided.")
+		}
 
-		DownloadAndSaveFile()
+		objectKeyToDownload := os.Args[2]
+		if err := DownloadFile(objectKeyToDownload); err != nil {
+			return err
+		}
 
 	case "list":
-		objSlice, err := GetObjectKeys()
+		objInfoSlice, err := GetObjectKeys()
 		if err != nil {
 			return err
 		}
 
-		for _, objKey := range *objSlice {
-			fmt.Println(objKey)
+		for _, objInfo := range *objInfoSlice {
+			fmt.Printf("%s %s\n", utils.LogColourSprintf(fmt.Sprintf("[%.2fMB]", float64(objInfo.Size)/float64(1000_000)), "yellow", false), objInfo.ObjectKey)
 		}
 
 		// Uploads file not dir YET
@@ -311,16 +420,15 @@ func handleCliArgs() error {
 			return fmt.Errorf("No target path provided.")
 		}
 
-		fileToUpload := os.Args[2]
-		fileToUpload, err := filepath.Abs(fileToUpload)
+		targetToUpload := os.Args[2]
+		targetToUpload, err := filepath.Abs(targetToUpload)
 		if err != nil {
 			return fmt.Errorf("Error finding file. %s", err.Error())
 		}
 
-		UploadFile(fileToUpload)
-
-		fmt.Println("File uploaded successfully.")
-		utils.ColourPrint("Uploaded successfully.", "green")
+		if err := UploadFile(targetToUpload); err != nil {
+			return err
+		}
 
 	case "dir":
 		if len(os.Args) < 3 {
@@ -339,52 +447,73 @@ func handleCliArgs() error {
 	return nil
 }
 
-func DownloadAndSaveFile() {
-	file, err := DownloadFile(s3Config.Bucket_name, "public/notes/"+"info.json", s3Config.Bucket_region)
+func DownloadFile(fileObjectKey string) error {
+	objInfo, err := ObjectKeyExists(fileObjectKey)
 	if err != nil {
-		log.Println("Error downloading file", err.Error())
-		return
+		return err
 	}
 
-	createDirPath := "./syncFolder/"
-	// Create the required path
-	err = os.MkdirAll(createDirPath, os.ModePerm)
-	if err != nil {
-		panic(err)
+	if objInfo == nil {
+		return fmt.Errorf("Object key does not exist in the bucket.")
 	}
 
-	if err := os.WriteFile(createDirPath+"file.txt", file, 0644); err != nil {
-		log.Println("Error writing to file")
-		return
+	fileBaseName := filepath.Base(fileObjectKey)
+
+	var userConfirmRes string
+	fmt.Printf("Downloading file %s (%.2fMB)\nContinue? (y/n)\n", objInfo.ObjectKey, float64(objInfo.Size)/float64(1000_000))
+	fmt.Scan(&userConfirmRes)
+
+	if userConfirmRes == "y" || userConfirmRes == "yes" || userConfirmRes == "Yes" || userConfirmRes == "Y" {
+		// Depending on the size of file, choose the upload method
+		// Right now a large file is ~100MB
+		if objInfo.Size <= LargeFileSize {
+			if err = DownloadNormalFile(fileObjectKey, fileBaseName); err != nil {
+				return fmt.Errorf("Could not download. %s", err.Error())
+			}
+
+		} else {
+			if err = DownloadLargeFile(fileObjectKey, fileBaseName); err != nil {
+				return fmt.Errorf("Could not download. %s", err.Error())
+			}
+		}
+		utils.ColourPrint("File downloaded successfully", "green")
+
+	} else {
+		fmt.Println(utils.LogColourSprintf("Aborted", "red", false))
 	}
+
+	return nil
 }
 
-func DownloadFile(bucketName string, objPath string, region string) ([]byte, error) {
-	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(region))
-	if err != nil {
-		return nil, err
-	}
-
-	client := s3.NewFromConfig(cfg)
-
-	fmt.Println("BName", bucketName)
-	fmt.Println("ObPath", objPath)
-	fmt.Println("Region", region)
+func DownloadNormalFile(fileObjectKey string, fileBaseName string) error {
 	output, err := client.GetObject(context.TODO(), &s3.GetObjectInput{
-		Bucket: aws.String(bucketName),
-		Key:    aws.String(objPath),
+		Bucket: &s3Config.Bucket_name,
+		Key:    &fileObjectKey,
 	})
 
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	defer output.Body.Close()
 
-	body, err := io.ReadAll(output.Body)
+	file, err := os.Create("./downloads/" + fileBaseName)
 	if err != nil {
-		return nil, err
+		return fmt.Errorf("Could not create file. %s", err.Error())
 	}
 
-	return body, nil
+	defer file.Close()
+	body, err := io.ReadAll(output.Body)
+	if err != nil {
+		return fmt.Errorf("Could not read object output. %s", err.Error())
+	}
+
+	_, err = file.Write(body)
+
+	return nil
+}
+
+func DownloadLargeFile(fileObjectKey string, fileBaseName string) error {
+	// TODO
+	return nil
 }
